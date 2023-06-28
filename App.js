baee3fa1eb26388e5dbd77ae8ebb59dd58d09224
leandro1990 <gomez.leandro1990@gmail.com>
2023-06-28 14:59:11 -0300

    handle task and task list in app js and implement input a…

--- App.js
+++ App.js
@@ -3,14 +3,31 @@ import { styles } from './styles';
 import { useState } from 'react';
 
 export default function App() {
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState([])
   const [borderColor, setBorderColor] = useState('#C5C9E7')
 
   const onHandlerFocus = () => {
     setBorderColor('#424D9E')
   }
-
   const onHandlerBlur = () => {
     setBorderColor('#C5C9E7')
+  }
+
+  const onHandlerChangeText = (text) => {
+    setTask(text)
+  }
+
+  const onHandlerCreateTask = () => {
+    setTasks([
+      ...tasks,
+      {
+        id: Date.now().toString(),
+        value: task
+      }
+    ])
+
+    setTask('')
   }
   return (
     <SafeAreaView style={styles.safeArea}>
@@ -26,8 +43,19 @@ export default function App() {
             placeholderTextColor='#C5C9E7'
             onFocus={onHandlerFocus}
             onBlur={onHandlerBlur}
+            onChangeText={onHandlerChangeText}
+            value={task}
           />
-          <Button title='Create' color='#424D9E' />
+          <Button disabled={task.length === 0} title='Create' color='#424D9E' onPress={onHandlerCreateTask}/>
+        </View>
+        <View style={styles.listContainer}>
+          {
+            tasks.map((item) => (
+              <View style={styles.containerItem} key={item.id}>
+                <Text style={styles.listItem}>{item.value}</Text>
+              </View>
+            ))
+          }
         </View>
       </View>
     </SafeAreaView>
