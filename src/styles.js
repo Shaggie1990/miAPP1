be52bf8c80802a88c19a2b1bb39b25daf9f992f2
leandro1990 <gomez.leandro1990@gmail.com>
2023-07-03 23:01:15 -0300

    add folder structure and create components and implement in app js

--- src/styles.js
+++ src/styles.js
@@ -10,44 +10,11 @@ export const styles = StyleSheet.create({
     marginHorizontal: 20,
     paddingTop: StatusBar.currentHeight,
   },
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  input: {
-    flex: 0.95,
-    borderWidth: 1,
-    borderRadius: 10,
-    paddingHorizontal: 10,
-    color: "#212121",
-    fontSize: 14,
-  },
+
   listContainer: {
     marginTop: 25,
   },
-  containerItem: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingVertical: 20,
-    backgroundColor: "#D4D7ED",
-    paddingHorizontal: 10,
-    borderRadius: 10,
-    color: "#212121",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.22,
-    shadowRadius: 2.22,
-    elevation: 3,
-  },
-  listItem: {
-    fontSize: 15,
-    fontWeight: "bold",
-    color: "#212121",
-  },
+
   list: {
     gap: 15,
     paddingBottom: 20,
